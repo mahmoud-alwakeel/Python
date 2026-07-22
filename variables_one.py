8ab348eca8e9a8b305aa452be195879eb262e856
mahmoud alwakeel <mahmoudalwakeel200@gmail.com>
2026-07-22 14:35:43 +0300

    first commit

---
+++ variables_one.py
@@ -0,0 +1,16 @@
+myVariable = "My Variable"
+
+_myVariable = "My Variable 2"
+
+# We cannot start a variable name with a number, so the following line would cause an error if uncommented
+# 100myVariable = "My Variable 2"
+my3Variable3 = "My Variable 3"
+
+# We cannot use a hyphen in a variable name, so the following line would cause an error if uncommented
+# my-Variable3 = "My Variable 4"
+
+print(myVariable)
+print(_myVariable)
+# print(100myVariable)
+print(my3Variable3)
+# print(my-Variable3)
